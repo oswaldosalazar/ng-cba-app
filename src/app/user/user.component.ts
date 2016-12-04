49a87e937c0833dd94ff9f55add1d5f64d23e290
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-12-03 19:41:59 -0700

    Added user help

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,47 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from '../login/login.component'
-import { FacebookService, FacebookInitParams, FacebookLoginResponse, FacebookAuthResponse } from 'ng2-facebook-sdk/dist';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
-  providers: [FacebookService]
+  styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  user: any;
-  userReady: boolean = false;
-  userPicture = 'something';
 
+  constructor( ) { }
 
-  constructor( public fb: FacebookService ) {
-    this.fb.api("/me?fields=name,gender")
-      .then(function(user) {
-        console.log("FROM USER COMPONENT", user)
-        user.picture = "https://graph.facebook.com/" + user.id + "/picture?type=large";
-        this.userPicture = user.picture;
-      })
-    }
-
-    FBLogout(): void {
-      this.fb.getLoginStatus().then(
-      (response: FacebookLoginResponse) => {
-        if (response.status === 'connected') {
-          console.log("button logout",response)
-          this.fb.logout()
-          .then(function(response) {
-            console.log(response)
-          })
-          .catch(function(error) {
-            console.log(error)
-          })
-       }
-     })
-  };
-
-
-  ngOnInit( ) {
-
-  }
+  ngOnInit( ) { }
 }
