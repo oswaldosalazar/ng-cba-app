981504bb67592be57b6eb47b01d13b48799c77e1
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-12-03 19:58:06 -0700

    Changed users settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,15 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { StudentsComponent } from './students/students.component';
 import { SavedComponent } from './saved/saved.component';
+import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
   { path: '',   component: HomeComponent },
   { path: 'login',  component: LoginComponent },
-  { path: 'user',   component: UserComponent },
   { path: 'students',   component: StudentsComponent },
   { path: 'saved',   component: SavedComponent },
   { path: 'user',   component: UserComponent }
